450177a1723d0e149b58d9fd54f952aa6826be29
Flandia Yingman <Flandia_YingM@hotmail.com>
2022-02-18 17:21:29 +0800

    feat: add new options

--- internal/app/app.go
+++ internal/app/app.go
@@ -3,7 +3,9 @@ package app
 import (
 	"arkwaifu/internal/app/config"
 	"arkwaifu/internal/app/infra"
-	repo2 "arkwaifu/internal/app/repo"
+	"arkwaifu/internal/app/repo"
+	"arkwaifu/internal/app/service"
+	"arkwaifu/internal/app/updateloop"
 	"context"
 	"fmt"
 	"go.uber.org/fx"
@@ -14,15 +16,22 @@ func ProvideOptions() []fx.Option {
 		fx.Provide(config.ProvideConfig),
 		fx.Provide(infra.ProvidePostgres),
 		fx.Provide(
-			repo2.NewAvgRepo,
-			repo2.NewAvgGroupRepo,
+			repo.NewResVersionRepo,
+			repo.NewAvgRepo,
+			repo.NewAvgGroupRepo,
+		),
+		fx.Provide(
+			service.NewAvgService,
+		),
+		fx.Provide(
+			updateloop.NewUpdateLoopController,
 		),
 		//...
 	}
 	return opts
 }
 
-func Run(avgRepo *repo2.AvgRepo, avgGroupRepo *repo2.AvgGroupRepo) {
+func Run(avgRepo *repo.AvgRepo, avgGroupRepo *repo.AvgGroupRepo) {
 	id, err := avgGroupRepo.GetAvgGroupByID(context.Background(), "act15side")
 	if err != nil {
 		panic(err)
