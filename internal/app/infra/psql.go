ede16047bb7556385defc0a3f5e31cf69dd8f46f
Flandia Yingman <Flandia_YingM@hotmail.com>
2022-02-19 15:34:01 +0800

    refactor: move Repo from fiber.go to psql.go

--- internal/app/infra/psql.go
+++ internal/app/infra/psql.go
@@ -4,6 +4,7 @@ import (
 	"arkwaifu/internal/app/config"
 	"context"
 	"database/sql"
+	"github.com/pkg/errors"
 	"github.com/uptrace/bun"
 	"github.com/uptrace/bun/dialect/pgdialect"
 	"github.com/uptrace/bun/driver/pgdriver"
@@ -26,4 +27,61 @@ func ProvidePostgres(config *config.Config) (*bun.DB, error) {
 		return nil, err
 	}
 	return db, nil
+}
+
+type Repo struct {
+	// conn is for injection. e.g., Atomic.
+	conn *bun.DB
+	// DB is for normal database operations.
+	DB bun.IDB
+}
+
+func NewRepo(db *bun.DB) Repo {
+	return Repo{
+		conn: db,
+		DB:   db,
+	}
+}
+
+func (r *Repo) BeginTx(ctx context.Context) (err error) {
+	tx, err := r.conn.BeginTx(ctx, &sql.TxOptions{})
+	if err != nil {
+		return err
+	}
+	r.DB = tx
+	return nil
+}
+
+func (r *Repo) Commit() error {
+	tx, ok := r.DB.(bun.Tx)
+	if !ok {
+		return errors.New("The transaction hasn't begun.")
+	}
+	err := tx.Commit()
+	if err != nil {
+		return err
+	}
+	r.DB = r.conn
+	return nil
+}
+
+func (r *Repo) Rollback() error {
+	tx, ok := r.DB.(bun.Tx)
+	if !ok {
+		return errors.New("The transaction hasn't begun.")
+	}
+	err := tx.Rollback()
+	if err != nil {
+		return err
+	}
+	r.DB = r.conn
+	return nil
+}
+
+func (r *Repo) EndTx(err error) error {
+	if err != nil {
+		return r.Rollback()
+	} else {
+		return r.Commit()
+	}
 }
