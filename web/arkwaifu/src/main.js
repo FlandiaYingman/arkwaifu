8fe26c64aa8f18bf74aa6a2fcfdb600e332cac9c
Flandia Yingman <Flandia_YingM@hotmail.com>
2022-03-10 20:59:47 +0800

    build(frontend): install lodash

--- web/arkwaifu/src/main.js
+++ web/arkwaifu/src/main.js
@@ -2,8 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import router from './router'
+import _ from 'lodash'
 
 Vue.prototype.$API_URL = process.env.VUE_APP_API_URL
+
+Object.defineProperty(Vue.prototype, "_", { value: _ });
 
 new Vue({
   vuetify,
