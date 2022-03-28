2cd85e49e20e9bb276e6dc48f6906b34a94718c5
Flandia Yingman <Flandia_YingM@hotmail.com>
2022-03-28 13:28:51 +0800

    fix(frontend): provide further description for AssetView

--- web/arkwaifu/src/views/assets/AssetView.vue
+++ web/arkwaifu/src/views/assets/AssetView.vue
@@ -13,6 +13,12 @@
     <p class="text-body-1">
       {{ $t('variants.desc') }}
     </p>
+    <p class="text-body-1">
+      {{ $t('variants.descVariants') }}
+    </p>
+    <p class="text-body-1">
+      {{ $t('variants.descSuperResolution') }}
+    </p>
     <v-expansion-panels>
       <v-expansion-panel
         v-for="variant in variants"
@@ -66,7 +72,9 @@ export default {
   "en": {
     "variants": {
       "title": "Variants",
-      "desc": "These are the variants of the asset. Note that some files are huge (~10MB) and may take a while to load."
+      "desc": "These are the variants of the asset. Note that some files are huge (~10MB) and may take a while to load.",
+      "descVariants": "The Raw Image is the original image extracted from the game. The Super-Resolution Images are the high-resolution versions of image after super-resolution with different models (Real-CUGAN, Real-ESRGAN, etc.). Thumbnail is the low-resolution and low-quality version of the image. ",
+      "descSuperResolution": "Real-CUGAN and Real-ESRGAN are two different models. There are slightly differences between the produced images. For me, Real-CUGAN looks more massive, and Real-ESRGAN looks more fine and smooth. "
     },
     "variant": {
       "img": "Raw Image",
@@ -78,13 +86,15 @@ export default {
   "zh": {
     "variants": {
       "title": "变体",
-      "desc": "这些是资源的变体。注意，一些文件是很大的（~10MB），可能需要一段时间来加载。"
+      "desc": "这些是资源的变体。注意，一些文件是很大的（~10MB），可能需要一段时间来加载。",
+      "descVariants": "原始图指从游戏中提取的原始图像；超分辨率指使用不同模型（Real-CUGAN、Real-ESRGAN 等）进行超分辨率后的高清版原始图像；缩略图指原始图像降低分辨率和质量后的缩略图。",
+      "descSuperResolution": "Real-CUGAN 和 Real-ESRGAN 是两个不同的模型，它们的超分辨率效果有细微的不同。就作者本人而言，Real-CUGAN 更厚重，而 Real-ESRGAN 更细腻。"
     },
     "variant": {
-    "img": "原始图",
-    "timg": "缩略图",
-    "real-esrgan": "超分辨率: Real-ESRGAN",
-    "real-cugan": "超分辨率: Real-CUGAN"
+      "img": "原始图",
+      "timg": "缩略图",
+      "real-esrgan": "超分辨率: Real-ESRGAN",
+      "real-cugan": "超分辨率: Real-CUGAN"
+    }
   }
-}
 }</i18n>
