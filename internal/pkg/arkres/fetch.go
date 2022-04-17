40cd96a9919e6fb61dd57276ddedca51375fc553
Flandia Yingman <Flandia_YingM@hotmail.com>
2022-04-17 21:02:30 +0800

    fix(backend): use retry queue to prevent error during fetching

--- internal/pkg/arkres/fetch.go
+++ internal/pkg/arkres/fetch.go
@@ -2,14 +2,15 @@ package arkres
 
 import (
 	"context"
+	"path/filepath"
+
 	"github.com/cavaliergopher/grab/v3"
 	"github.com/pkg/errors"
 	log "github.com/sirupsen/logrus"
-	"path"
-	"path/filepath"
 )
 
-const poolSize = 24
+const poolSize = 16
+const retryPoolSize = 1
 
 // fetch fetches the resources specified by srcs into dst.
 // dst must exist.
@@ -31,21 +32,51 @@ func fetch(ctx context.Context, srcs []Info, dst string) error {
 
 	num := 0
 	total := len(requests)
+	requests = nil
 	for response := range responses {
-		srcFile := path.Clean(response.Request.URL().String())
+		srcFile := response.Request.URL().String()
+		dstFile := filepath.ToSlash(filepath.Clean(response.Filename))
+
+		err := response.Err()
+		if err != nil {
+			req := response.Request
+			req = req.WithContext(ctx)
+			requests = append(requests, req)
+			log.WithFields(log.Fields{
+				"num":   num,
+				"total": total,
+				"src":   srcFile,
+				"dst":   dstFile,
+			}).Info("failed to fetch resource; appending it to retry queue")
+		} else {
+			log.WithFields(log.Fields{
+				"num":   num,
+				"total": total,
+				"src":   srcFile,
+				"dst":   dstFile,
+			}).Info("fetched resource")
+		}
+		num++
+	}
+
+	responses = grab.DefaultClient.DoBatch(retryPoolSize, requests...)
+
+	num = 0
+	total = len(requests)
+	for response := range responses {
+		srcFile := response.Request.URL().String()
 		dstFile := filepath.ToSlash(filepath.Clean(response.Filename))
 
 		err := response.Err()
 		if err != nil {
 			return errors.Wrapf(err, "srcFile：%v; dstFile: %v", srcFile, dstFile)
 		}
-
 		log.WithFields(log.Fields{
 			"num":   num,
 			"total": total,
 			"src":   srcFile,
 			"dst":   dstFile,
-		}).Info("fetched resource")
+		}).Info("fetched resource in retry queue")
 		num++
 	}
 	return nil
