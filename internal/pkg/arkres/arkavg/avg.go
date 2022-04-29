9820d58fcb4dd59dd151477d22051f117a0d1054
Flandia Yingman <Flandia_YingM@hotmail.com>
2022-04-29 12:55:36 +0800

    feat(backend): support AVG character assets in avg module

--- internal/pkg/arkres/arkavg/avg.go
+++ internal/pkg/arkres/arkavg/avg.go
@@ -2,11 +2,13 @@ package arkavg
 
 import (
 	"fmt"
-	"github.com/pkg/errors"
-	"go.mongodb.org/mongo-driver/bson"
 	"os"
 	"path/filepath"
 	"regexp"
+	"strconv"
+
+	"github.com/pkg/errors"
+	"go.mongodb.org/mongo-driver/bson"
 )
 
 type Avg struct {
@@ -76,11 +78,6 @@ func GetAvg(resDir string, prefix string) (Avg, error) {
 	return Avg{Groups: groups}, nil
 }
 
-var (
-	imageRegexp      = regexp.MustCompile(`(?i)\[Image\(.*?image="(.*?)".*?\)]`)
-	backgroundRegexp = regexp.MustCompile(`(?i)\[Background\(.*?image="(.*?)".*?\)]`)
-)
-
 func GetStoryAssets(resDir string, prefix string, story Story) ([]Asset, error) {
 	txt, err := GetStoryTxt(resDir, prefix, story)
 	if err != nil {
@@ -88,22 +85,16 @@ func GetStoryAssets(resDir string, prefix string, story Story) ([]Asset, error) 
 	}
 
 	var assets []Asset
-	assets = append(assets, findAssetsFromTxt(txt, KindImage, imageRegexp)...)
-	assets = append(assets, findAssetsFromTxt(txt, KindBackground, backgroundRegexp)...)
+	assets = append(assets, findPicAssetsFromTxt(txt, KindImage, imageRegexp)...)
+	assets = append(assets, findPicAssetsFromTxt(txt, KindBackground, backgroundRegexp)...)
+
+	charAssets, err := findCharAssetsFromTxt(txt)
+	if err != nil {
+		return nil, err
+	}
+	assets = append(assets, charAssets...)
 
 	return assets, nil
-}
-
-func findAssetsFromTxt(txt string, kind Kind, regexp *regexp.Regexp) []Asset {
-	var assets []Asset
-	matches := regexp.FindAllStringSubmatch(txt, -1)
-	for _, match := range matches {
-		assets = append(assets, Asset{
-			Name: match[1],
-			Kind: kind,
-		})
-	}
-	return assets
 }
 
 func GetStoryTxt(resDir string, prefix string, story Story) (string, error) {
@@ -115,4 +106,89 @@ func GetStoryTxt(resDir string, prefix string, story Story) (string, error) {
 		return "", err
 	}
 	return string(txtContent), nil
+}
+
+func findPicAssetsFromTxt(txt string, kind Kind, regexp *regexp.Regexp) []Asset {
+	var assets []Asset
+	matches := regexp.FindAllStringSubmatch(txt, -1)
+	for _, match := range matches {
+		assets = append(assets, Asset{
+			Name: match[1],
+			Kind: kind,
+		})
+	}
+	return assets
+}
+
+var (
+	imageRegexp      = regexp.MustCompile(`(?i)\[Image\(.*?image="(.*?)".*?\)]`)
+	backgroundRegexp = regexp.MustCompile(`(?i)\[Background\(.*?image="(.*?)".*?\)]`)
+
+	// characterRegexp matches the character directive in the story text.
+	// 	Example: "[character(name="avg_npc_415_1#5$1",name2="char_011_talula_1#3",focus=2)]"
+	//  group 1: the name (including # and $) of the 1st character, which is "avg_npc_415_1#5$1".
+	//	group 2 (optional): the name (including # and $) of the 2nd character, which is "char_011_talula_1#3".
+	characterRegexp = regexp.MustCompile(`(?i)(?U)\[Character\(.*name="(.*)".*(?:name2="(.*)".*)?\)\]`)
+
+	// characterNameRegexp matches the name of a character.
+	//
+	// 	Example: "avg_103_angel_1#11$1"
+	// 	group 1: the name of the character, which is "avg_103_angel_1".
+	// 	group 2 (optional): the face number of the character (after the hash sign), which is "11".
+	// 	group 3 (optional): the body number of the character (after the dollar sign), which is "1".
+	characterNameRegexp = regexp.MustCompile(`^(.*?)(?:#(\d+))?(?:\$(\d+))?$`)
+)
+
+func findCharAssetsFromTxt(txt string) ([]Asset, error) {
+	var assets []Asset
+	matches := characterRegexp.FindAllStringSubmatch(txt, -1)
+	for _, match := range matches {
+		// match[1] is "name", required
+		caName, err := normalizeCharAssetName(match[1])
+		if err != nil {
+			return nil, err
+		}
+		assets = append(assets, Asset{
+			Name: caName,
+			Kind: KindCharacter,
+		})
+
+		// match[2] is "name2", optional
+		if match[2] != "" {
+			caName, err := normalizeCharAssetName(match[2])
+			if err != nil {
+				return nil, err
+			}
+			assets = append(assets, Asset{
+				Name: caName,
+				Kind: KindCharacter,
+			})
+		}
+	}
+	return assets, nil
+}
+
+func normalizeCharAssetName(name string) (string, error) {
+	matches := characterNameRegexp.FindStringSubmatch(name)
+	if len(matches) == 0 {
+		return "", fmt.Errorf("invalid character name: %v", name)
+	}
+
+	var err error
+	faceNum, bodyNum := 1, 1 // default values are 1
+	if matches[2] != "" {
+		faceNum, err = strconv.Atoi(matches[2])
+		if err != nil {
+			return "", fmt.Errorf("invalid character face number: %v", matches[2])
+		}
+	}
+	if matches[3] != "" {
+		bodyNum, err = strconv.Atoi(matches[3])
+		if err != nil {
+			return "", fmt.Errorf("invalid character face number: %v", matches[3])
+		}
+	}
+
+	name = matches[1]
+	return fmt.Sprintf("%s#%d$%d", name, faceNum, bodyNum), nil
 }
