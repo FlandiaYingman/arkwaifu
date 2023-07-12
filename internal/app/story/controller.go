10476e9fea6102d7e2cb504c3ca72f8a40d65792
Flandia Yingman <flandia_yingm@hotmail.com>
2023-07-12 14:24:32 +0800

    refactor(story): remove temporary GetArts endpoint

--- internal/app/story/controller.go
+++ internal/app/story/controller.go
@@ -20,9 +20,6 @@ func registerController(c *controller, router fiber.Router) {
 	router.Get(":server/story-groups/:id", c.GetGroup)
 	router.Get(":server/stories", c.GetStories)
 	router.Get(":server/stories/:id", c.GetStory)
-
-	router.Get(":server/arts", c.GetArts)
-	router.Get(":server/arts/:id", c.GetArts) // TODO
 }
 
 func (c *controller) GetGroups(ctx *fiber.Ctx) error {
@@ -90,30 +87,4 @@ func (c *controller) GetStory(ctx *fiber.Ctx) error {
 	}
 
 	return ctx.JSON(story)
-}
-
-func (c *controller) GetArts(ctx *fiber.Ctx) error {
-	server, err := ark.ParseServer(ctx.Params("server"))
-	if err != nil {
-		return fiber.ErrBadRequest
-	}
-
-	pictureArts, err := c.service.GetPictureArts(server)
-	if err != nil {
-		return err
-	}
-	characterArts, err := c.service.GetCharacterArts(server)
-	if err != nil {
-		return err
-	}
-
-	arts := make([]any, 0)
-	for _, art := range pictureArts {
-		arts = append(arts, art)
-	}
-	for _, art := range characterArts {
-		arts = append(arts, art)
-	}
-
-	return ctx.JSON(arts)
 }
