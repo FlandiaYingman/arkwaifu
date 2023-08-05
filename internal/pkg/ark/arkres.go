e254569f2967f1993c10ccdd36f6d2f26da4d082
Flandia Yingman <flandia_yingm@hotmail.com>
2023-08-05 18:39:12 +0800

    refactor(ark): remove unused files

--- internal/pkg/ark/arkres.go
+++
@@ -1,6 +0,0 @@
-// Package ark provides functions to retrieve resources of the game Arknights.
-// This package only do fetching, unzipping, unpacking and decrypting resources.
-// To do other works, such as parsing BSON or JSON tables or processing graphics assets, use the subpackages.
-//
-// tools/extractor is required.
-package ark
